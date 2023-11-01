ce591920ea2f5b4e0592df88344c7e8d333dd420
Ubabar786 <babaruzair@gmail.com>
2023-11-01 23:53:50 +0000

    this commit has the feature sun added

--- src/test/java/SolarSystem/main.java
+++ src/test/java/SolarSystem/main.java
@@ -7,5 +7,11 @@ public class main {
         solarsys.Moon="2";
         solarsys.Planets="2";
         solarsys.Stars="10";
+
+        //***FeatureSun***
+        FeatureSun sun=new FeatureSun();
+        sun.name="panther";
+        sun.colour="black";
+        sun.temp="-1223124kelvin";
     }
 }
