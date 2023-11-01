21496cbfd020df18f5932469eb74561933189177
Ubabar786 <babaruzair@gmail.com>
2023-11-01 23:22:31 +0000

    this commit has the basic structure of solar system

---
+++ src/test/java/SolarSystem/main.java
@@ -0,0 +1,11 @@
+package SolarSystem;
+
+public class main {
+    public static void main(String[] args) {
+        solarSystembasics solarsys = new solarSystembasics();
+        solarsys.Sun="1";
+        solarsys.Moon="2";
+        solarsys.Planets="2";
+        solarsys.Stars="10";
+    }
+}
---
+++ src/test/java/SolarSystem/solarSystembasics.java
@@ -0,0 +1,9 @@
+package SolarSystem;
+
+public class solarSystembasics {
+    String Sun;
+    String Moon;
+    String Stars;
+    String Planets;
+
+}
